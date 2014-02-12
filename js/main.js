e792f9426663328aed563e86b09cfba2600adce1
Ed Wheeler <ew59@nyu.edu>
2014-02-11 19:43:29 -0500

    CORS problems ugh

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 var insta = angular.module('insta', []);
 
+insta.config(['$httpProvider', function($httpProvider) {
+	$httpProvider.defaults.withCredentials = true;
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	}
+]);
+
 function AppController($scope, $http) {
 	'use strict';	
 	
 	$scope.search = function() {
-		$http.get('js/ig.json').success(function (data) {		
-			$scope.photos = data.data;	
+		$http({
+			method: 'GET',
+			url:'https://api.instagram.com/v1/tags/' + $scope.searchKeyword + '/media/recent?client_id=1ff71fd8d0ef4b3ea058f93ee4852154&callback=wheels', 
+			headers:{
+				'Access-Control-Allow-Origin': '*'
+			}
+			
+			}).success(function (data) {						
+				$scope.photos = data.data;	
 			//$scope.tags = data.data.tags;
 			
 			//console.log($scope.tags);
