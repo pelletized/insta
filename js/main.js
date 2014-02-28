eb0ed754d84f7c39d7fe2f577d8dbb68f88dabe2
Ed Wheeler <ew59@nyu.edu>
2014-02-28 18:04:26 -0500

    display popular photos on load

--- js/main.js
+++ js/main.js
@@ -10,8 +10,26 @@ insta.config(['$httpProvider', function($httpProvider) {
 function AppController($scope, $http) {
 	'use strict';	
 	
+	var clientId = "1ff71fd8d0ef4b3ea058f93ee4852154";
+		
+	//on load
+	$scope.onload = function() {
+		$http.jsonp('https://api.instagram.com/v1/media/popular?client_id=' + clientId + '&callback=JSON_CALLBACK').success(function (data) {						
+					$scope.photos = data.data;	
+					//var index = $scope.photos.indexOf(tags);
+					//$scope.created = $scope.photos.created_time;
+				
+					for (var i = 0; i < $scope.photos.length; i++) {
+						console.log($scope.photos[i].tags);
+					}
+				
+				//console.log($scope.created);
+			});		
+	};
+	
+	//search 
 	$scope.search = function() {
-		$http.jsonp('https://api.instagram.com/v1/tags/' + $scope.searchKeyword + '/media/recent?client_id=1ff71fd8d0ef4b3ea058f93ee4852154&callback=JSON_CALLBACK').success(function (data) {						
+		$http.jsonp('https://api.instagram.com/v1/tags/' + $scope.searchKeyword + '/media/recent?client_id=' + clientId +'&callback=JSON_CALLBACK').success(function (data) {						
 				$scope.photos = data.data;	
 			//$scope.tags = data.data.tags;
 			
